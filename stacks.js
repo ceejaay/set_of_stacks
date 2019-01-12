1026b8f72b925d8043c82f773fca35c10a0d9491
Chad Jemmett <chad.jemmett@gmail.com>
2019-01-12 15:34:23 -0700

    Wrote the first part of the set of stacks

---
+++ stacks.js
@@ -0,0 +1,38 @@
+class StackSet {
+  constructor(maxSize) {
+    if(arguments.length < 1) {
+      throw new Error ("Whoop, max size is required.")
+    }
+    this.stacks = [[]]
+    this.maxSize = maxSize
+  }
+
+
+  push(value) {
+    if ( this.stacks[this.stacks.length - 1].length === this.maxSize ) {
+      this.stacks.push([])
+    }
+    this.stacks[this.stacks.length -1].push(value)
+  }
+
+  value() {
+    return this.stacks[0].length
+  }
+}
+
+
+
+
+const stack = new StackSet(3)
+stack.push(2)
+stack.push('f')
+stack.push(4)
+stack.push(4)
+stack.push(4)
+stack.push(4)
+stack.push(4)
+
+console.log(stack.value())
+console.log(stack)
+
+  
